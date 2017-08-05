0b9588482722855a5afea4e194c0367a1376a5df
Sylvie Ong <sylvie.ong@noisyidea.com>
2017-08-04 20:50:42 -0500

    Ported over original files to *_merged.py files

---
+++ post_ocr_dso/post_ocr_dso_merged.py
@@ -0,0 +1,231 @@
+"""
+Map field names that appear in tool label to one of 9 required fields (or append
+to list of extra_info.
+Find and validate field values.
+Return results as Pass/Fail for each field name and value, and for the label as a whole
+(full and restricted list of fields)
+"""
+
+from collections import Sequence
+from collections import OrderedDict
+
+import pandas as pd
+import sys
+import string
+from nltk.metrics import distance
+import numpy as np
+
+'''
+validate_label that is returned in the result of the DSO is a dict() with this structure:
+'''
+default_output_result= {
+ "label": {
+    "present": True,
+    "standard_full": "Fail",
+    "standard_restricted": "Fail",
+    "standard_full_fail_reason": " ",
+    "standard_restricted_fail_reason": " "
+ },
+ "required_fields":{
+    "property_of_unit": {
+             "literal": ["",""],
+             "standard":[ "Fail", "Fail"],
+              "confidence": [-1, -1]},
+    "inventory_number": {
+            "literal": ["", ""],
+            "standard": ["Fail", "Fail"],
+            "confidence": [-1, -1]},
+    "part_number" : {
+            "literal": ["", ""],
+            "standard": ["Fail", "Fail"],
+            "confidence": [-1, -1]},
+    "part_designation" : {
+            "literal": ["", ""],
+            "standard": ["Fail", "Fail"],
+            "confidence": [-1, -1]},
+
+    "project": {
+            "literal": ["", ""],
+            "standard": ["Fail", "Fail"],
+            "confidence": [-1, -1]},
+
+    "weight_kg" :  {
+            "literal": ["", ""],
+            "standard": ["Fail", "Fail"],
+            "confidence": [-1, -1]},
+
+    "dimension" :{
+            "literal": ["", ""],
+            "standard": ["Fail", "Fail"],
+            "confidence": [-1, -1]},
+
+    "issue_date" : {
+            "literal": ["", ""],
+            "standard": ["Fail", "Fail"],
+            "confidence": [-1, -1]},
+
+    "DUNS" : {
+            "literal": ["", ""],
+            "standard": ["Fail", "Fail"],
+            "confidence": [-1, -1]}
+
+  },
+  "extra_info": []
+}
+
+class ValidateFieldsDSO():
+    # TODO: enumerate set of 9 required fields
+
+    # -----------------------
+    # function match_literal_with_field()
+    # returns one of 9 fields
+    # what it does:
+    # match token with literal in dictionary
+    # dict_eight = { 'part_number' : [part number', ... ],
+    #          'inventory_number' : ['inventory number', ...],
+    #       }
+
+    # dict_property_of_unit
+
+    # algo
+    # go through each key in dict_eight and find max match with the literals in its list
+    # get the max match across the keys,
+    # if it is higher than a threshold, consider it a match, return the matched key
+    # else try to do smart matching wih literals in dict_property_of_unit,
+    # if there is a match, return property_of_unit
+    # else return None
+
+    # algo - more robust: to deal with 'AUDI Inventar-Nr2152601 -002\n\n', 'Teilenummen im 201 021',
+    # 'Eigentum der Audi AG'
+    # split token_to_match by space, keep appending the split results and
+    # trying to match with literals in the dictionary till the match score starts decreasing
+    # consider a match as found if the final match score is above a threshold
+    # if there is a match,
+    # return the matched field and the rest of the split results
+    # else return None
+
+    # how to match
+    # - convert everything to lower case
+    # - find some matching algo - levenstein distance?
+
+    required_keys_list = ['property_of_unit', 'inventory_number', 'part_number', 'part_designation', 'project',
+                          'weight_kg', 'dimension', 'issue_date', 'DUNS']
+
+    fieldname_variations = OrderedDict()
+    fieldname_variations['property_of_unit'] = ['property of unit', 'eigentum', 'eigentum der', 'kundeneigentum',
+                                                'eigentum firma']
+    fieldname_variations['inventory_number'] = ['inventory number', 'inventarnummer', 'invent-nr.', 'tool no.',
+                                                'wekrzeugnummer']
+    fieldname_variations['part_number'] = ['part no', 'part number', 'teilenummer',  'audi teilenummer']
+    fieldname_variations['part_designation'] = ['part designation', 'part name', 'description', 'teilebenennung',
+                                                'bezeichnung']
+    fieldname_variations['project'] = ['project', 'vehicle object', 'fahrzeugprojekt']
+    fieldname_variations['weight_kg'] = ['geewicht', 'swz gesamtgewicht', 'swzgesamtgewicht', 'tool weight', 'weight',
+                                         'serial tool weight kg']
+    fieldname_variations['dimension'] = ['dimension', 'dimension l w h', 'dimensions mm', 'abmessung']
+    fieldname_variations['issue_date'] = ['issue date', 'erstelldatum', 'werkzeugerstellung','tool construction date',
+                                          'baujahr', 'year of manuf', 'date of manufacture', 'year of manufacture']
+    fieldname_variations['DUNS'] = ['duns', 'hersteller']
+
+    def match_literal_fieldname_variations(self, literal_to_match):
+        fieldname_match_scores = []
+        for fieldname, variations in self.fieldname_variations.items():
+            g = lambda x: distance.edit_distance(x,self.normalize(literal_to_match))
+            variations_match_scores= [g(x) for x in variations]
+            fieldname_match_scores.append(min(variations_match_scores))
+        fieldname_match_scores_nparr = np.asarray(fieldname_match_scores)
+        return np.argmin(fieldname_match_scores_nparr), np.min(fieldname_match_scores_nparr)
+
+    def normalize(self,s):
+        for p in string.punctuation:
+            s = s.replace(p, '')
+        return s.lower().strip()
+
+    def map_literal_to_field(self, tokens_to_match):
+        #print(f'In function {sys._getframe().f_code.co_name}')
+
+        # match with literals in the dictionary
+        # iterate through fieldname_variations and for each key, find max match score with its list of variations
+        fieldname_min_index,  fieldname_min_score = self.match_literal_fieldname_variations(tokens_to_match)
+        #print(f'best matched field: {self.required_keys_list[fieldname_min_index ]}, score:{fieldname_min_score}')
+        print('best matched field: {}, score:{}'.format(self.required_keys_list[fieldname_min_index],fieldname_min_score))
+
+
+        return 'dummy_field', ['dummy', 'tokens', 'to', 'add']
+
+
+
+    def __init__(self, *args, **kwargs):
+        self.__dict__.update(kwargs)
+        super().__init__()
+
+    #def calculate(self, dataset: Sequence, *, res_config: RunResourceConfig = None):
+    def calculate(self, dataset: Sequence):
+
+        output_result_list = []
+
+        for item in dataset:
+            print('OCR output for label:')
+            print(item[2])
+            print(item[3])
+            input_word_list = item[2]
+            input_confidence_value = item[3]
+
+            output_result = default_output_result
+
+            # ------------------------
+
+            # ** for each line in input_word_list, find one of the 9 fields that it belongs to, or if not,
+            # append to extra_info **
+
+            # for each line:
+            #   if line has >1 box, get leftmost box as matching token, join the rest of the boxes with space
+            #  <--- still need to split leftmost by ":"
+            #   else split text by ":" and get leftmost token, join the rest of the boxes together with ":"
+
+            # get rid of \n\n, leading and trailing spaces
+
+            #   try to match the leftmost box with a literal in the dictionary - call match_literal_with_field()
+            #   if there is a match,
+            #       append token to output_result.<returned_field>['literal']
+            #       append rest of the line to output_result.<returned_field>['literal']
+            #       append 'Pass' to output_result.<returned_field>['standard']
+            #       if rest of the line is not empty or exists or there is a list:
+            #           if returned_field is DUNS or inventory_number, validate the rest of the line, append result to
+            #           output_result.<returned_field>['standard']
+            #           else append 'Pass' to output_result.<returned_field>['standard']
+            #       else append 'Fail' to output_result.<returned_field>['standard']
+            #   else append whole line to extra_info
+
+            # important - treat empty string returned from OCR, which is not the first string, as a value that was detected
+            #
+
+            for line in input_word_list:
+                #print(f'line: {line}')
+                print('line: {}'.format(line))
+
+
+                if len(line) > 1:
+                    pass
+                    # get leftmost box as matching token, join the rest of the boxes with space
+                    # split leftmost box with ":"
+
+                else:
+                    # split text by ":" and get leftmost token, join the rest of the boxes together with ":"
+                    line_tokens = line[0].split(":")
+                    tokens_to_match = line_tokens[0]
+                    tokens_to_match = tokens_to_match.strip()
+                    tokens_rest_of_line = ':'.join(line_tokens[1:])
+                    tokens_rest_of_line = tokens_rest_of_line.strip()
+
+                    #TODO get rid of \n at beginning, end and anywhere and replace with space?
+                    # tokens_rest_of_line.strip()
+                    #print(f'tokens_to_match: {tokens_to_match}')
+                    print('tokens_to_match: {}'.format(tokens_to_match))
+                    #print(f'tokens_rest_of_line: {tokens_rest_of_line}')
+                    print('tokens_rest_of_line: {}'.format(tokens_rest_of_line))
+                    field, tokens_to_add_to_rest_of_line = self.map_literal_to_field(tokens_to_match)
+
+            output_result_list.append(output_result)
+
+        return output_result_list
---
+++ post_ocr_dso/test_post_ocr_dso_merged.py
@@ -0,0 +1,147 @@
+
+import post_ocr_dso_merged
+
+
+class TestValidateFieldsDSO():
+
+    def setUp(self):
+        """
+        create dataset from tuples of output from ocr_dso
+        writes generator created from the dataset to self.data
+        :return:
+        """
+
+        # data output from ocr_dso looks like:
+        # label_detected, boxes, ocr.label_txt_pair, ocr.label_conf_pair
+        # result_list # list of tuples, where each tuple looks like: label_detected, boxes, ocr.label_txt_pair, ocr.label_conf_pair
+
+        # THE FILE NAME IS:  Labels/4_2_Blow_mold_MA_3202560_2.jpg
+        label_txt_pair0 = [ ['Teilebenennung: Montagevorrichtung', 'Aufschuss EFR-LZS'],
+                            ['D-U-N-S: 312968706'],
+                            ['AUDI lnventar-Nr2152601 -002'],
+                            ['Fahrzeugprojekt: P0 416'],
+                            ['Teilenummer: 8R0 201 021'],
+                            ['Eigentum der AUDI AG'],
+                            ['Erstelldatum: Nov. 2013'] ]
+        label_conf_pair0 =[ [[79, 83], [89, 80]],
+                            [[81, 82]],
+                            [[82, 64, 74]],
+                            [[79, 89, 78]],
+                            [[80, 88, 87, 85]],
+                            [[88, 91, 92, 90]],
+                            [[84, 88, 85]] ]
+
+        #THE FILE NAME IS: Labels / 4_2_Blow_mold_MA_3202560_8.jpg
+        label_txt_pair1 = [ ['Invenhmummer Audi: 2152601405'],
+        ['Teilebenennung: Ladungslrägor EFR'],
+        ['Werkzeugelshellung: Nov. 2013'],
+        ['Fahneugpcojekl: ro 416'],
+        ['Teilenummen im 201 021'],
+        ['Eigentum der Audi AG'] ]
+
+        label_conf_pair1 = [ [['67', '72', '56']],
+        [['74', '69', '78']],
+        [['69', '85', '71']],
+        [['71', '71', '74']],
+        [['74', '63', '88', '90']],
+        [['78', '75', '77', '76']] ]
+
+        # THE FILE NAME IS:  Labels/4_2_Extrusion_tool_MA_3284664_2.jpg
+        label_txt_pair2 = [ ['Abmessung:', '', 'HERST. DAT. 08/201677'],
+        ['', ''],
+        ['Eigentum der\n\n3 nt:', ''],
+        ['lnventamr.', ''],
+        ['D-U-N-S', ''],
+        ['', '', 'JEILE-NR. USM 103 210 CH'],
+        ['ErsIeHdufum:', ''],
+        ['bläbmonnung:', '', 'WK. NR.', '1012657'],
+        ['Gewicht:', '', 'EEZ. Druckprüileisle'] ]
+
+        label_conf_pair2 = [ [['69'], ['-1'], ['87', '91', '22']],
+[['-1'], ['-1']],
+[['73', '84', '56', '49'], ['-1']],
+[['64'], ['-1']],
+[['85'], ['-1']],
+[['-1'], ['-1'], ['68', '71', '78', '79', '84']],
+[['69'], ['-1']],
+[['47'], ['-1'], ['93', '86'], ['95', '78']],
+[['59'], ['-1'], ['49', '75']] ]
+
+        # THE FILE NAME IS:  Labels/4_2_Foaming_tool_MA_3173300.jpg
+        label_txt_pair3 = [ ['Audi Teilenummer/Partnumber', '8V5 012 109 C/D'],
+['Eigentum/Property', 'AUDI AG'],
+['Audi lnventarnummer/lnventorynumber', '2145323'],
+[''] ]
+
+        label_conf_pair3 = [[['94', '88'], ['93', '89', '89', '93']],
+[['86'], ['95', '91']],
+[['93', '89'], ['87']],
+[['-1']]]
+
+        # THE FILE NAME IS:  Labels/4_2_Foaming_tool_MA_3193397_3.jpg
+        label_txt_pair4 = [['Xvazuuqngmmer', '1616 60011958', 'SCHAUMWERKZEUG 5- NUTZEN'],
+['Ten f Nummer', '', ''],
+['Teil Bezeichnung', '- .v.\n\nU G SQHALTUNG HAND'],
+['KWV', '', 'Agusm\n\nls.-'],
+['Eigentum', 'AUDI AG'],
+['', '', 'Gewicht Ikgl', 'SchwindmaB D/o', '1.39', '225'],
+['', ''],
+['k 7 wimmenal', '6116 60011958', '1SCHAUK/TWERKZEUG 5- NUTZEN-'],
+['KWV', 'Y1L79l-Sm'],
+['WNkz-eugart', '64149ng\ntSCHAUMWERKZEUG 5- NUTZEN\n\nn de Anne nnnnnnen -'],
+['', 'Mmic dä x T x m hmm', '770x418x314', 'Gewicht Ikgl', '225'],
+['', ''],
+['Teilenummer: .BSO 863 878'],
+['WERKZEUGDATEN'],
+['lnventarnummer: 0702983001']]
+
+        label_conf_pair4 = [[['47', '95'], ['95', '88', '80'], ['95', '87', '92', '88']],
+[['70', '34', '80'], ['-1'], ['-1']],
+[['17', '73'], ['78', '59', '93', '93', '63', '82', '95', '95', '95']],
+[['54'], ['-1'], ['95', '44', '76']],
+[['82'], ['92', '89']],
+[['-1'], ['-1'], ['79', '72'], ['68', '73'], ['87'], ['90']],
+[['-1'], ['-1']],
+[['95', '72', '52', '48'], ['95', '88', '80'], ['47', '89', '49']],
+[['54'], ['47']],
+[['48'], ['18', '56', '93', '88', '79', '63', '61', '63', '65']],
+[['-1'], ['56', '51', '68', '75', '62', '66', '64'], ['80'], ['79', '72'], ['90']],
+[['-1'], ['-1']],
+[['84', '25', '82', '83']],
+[['84']],
+[['82', '78']]]
+
+        tuple0 = ( True, [1,2], label_txt_pair0, label_conf_pair0 )
+        tuple1 = ( True, [1,2], label_txt_pair1, label_conf_pair1 )
+        tuple2 = ( True, [1,2], label_txt_pair2, label_conf_pair2 )
+        tuple3 = ( True, [1,2], label_txt_pair3, label_conf_pair3 )
+        tuple4 = ( True, [1,2], label_txt_pair4, label_conf_pair4 )
+
+        result_list = [tuple0, tuple1, tuple2, tuple3, tuple4]
+
+        self.data = (result for result in result_list)
+
+        # note that in the dtg, the data passed out of the ocr DSO will include celph_file etc, and we would do:
+        # has_label, lines_cleaned, label_txt_pair, label_conf_pair = data['ocr_result']
+
+    def test_1(self):
+        print('debug validate fields dso')
+
+        #model_config = ValidateFieldsDSO_Config()
+        #model_dso = model_config.get_dso()
+        model_dso= post_ocr_dso_merged.ValidateFieldsDSO()
+        result = model_dso.calculate(self.data) #, res_config=res_config)
+
+        print('Result:')
+        for item in result:
+            print(item)
+
+
+
+        print('\ndone!')
+
+
+if __name__ == '__main__':
+    dso_tester=TestValidateFieldsDSO()
+    dso_tester.setUp()
+    dso_tester.test_1()
