9418fecd3e1c132f9d5125b777ba444757058089
Sylvie Ong <sylvie.ong@gmail.com>
2018-02-16 08:16:18 -0600

    bits and pieces

--- sandbox/test_name_of_module.py
+++ sandbox/test_name_of_module.py
@@ -1,9 +1,10 @@
 from test_metaclasses import print_fn as tm_print_fn
 import test_metaclasses
 
-print(f'import test_metaclasses')
-test_metaclasses.print_fn()
+def call_print_fn():
+	print(f'import test_metaclasses')
+	test_metaclasses.print_fn()
 
 
-print(f'from test_metaclasses import print_fn as tm_print_fn')
-tm_print_fn()
+	print(f'from test_metaclasses import print_fn as tm_print_fn')
+	tm_print_fn()
